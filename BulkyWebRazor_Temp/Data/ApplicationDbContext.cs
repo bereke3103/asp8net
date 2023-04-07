681646f9de7bf4b6aa7445662947447c10670854
Bereke Sakan <bereke3103@mail.ru>
2023-04-07 17:22:34 +0600

    razor pages

---
+++ BulkyWebRazor_Temp/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using BulkyWebRazor_Temp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWebRazor_Temp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id=1, Name="Action", DisplayOrder=21 },
+                new Category { Id=2, Name="Non Action", DisplayOrder=33 },
+                new Category { Id=3, Name="Me", DisplayOrder=79 }
+                );
+        }
+  
+    }
+}
---
+++ BulkyWebRazor_Temp/Migrations/20230407094256_addedData.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BulkyWebRazor_Temp.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "DisplayOrder", "Name" },
+                values: new object[,]
+                {
+                    { 1, 21, "Action" },
+                    { 2, 33, "Non Action" },
+                    { 3, 79, "Me" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
