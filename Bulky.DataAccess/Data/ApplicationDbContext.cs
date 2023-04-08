0e69183f4078963210e033ba931f5c1336fe5761
Bereke Sakan <bereke3103@mail.ru>
2023-04-08 15:12:35 +0600

    UI

---
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Bulky.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulky.DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "ACTION", DisplayOrder = 21 },
+                new Category { Id = 2, Name = "SEED", DisplayOrder = 5 },
+                new Category { Id = 3, Name = "TABLE", DisplayOrder = 11 }
+            );
+        }
+    }
+}
